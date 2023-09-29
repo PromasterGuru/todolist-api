9c3195c1673091a18f4dd63bd699fce25376ee4a
promasterguru <pmutondo12@gmail.com>
2023-09-29 23:18:55 +0300

    Update git workflow

--- api/settings.py
+++ api/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME')
+        'NAME': env('DB_NAME'),
         'USER':  env('DB_USER'),
         'PASSWORD':  env('DB_PASS'),
         'HOST':  env('HOST'),
