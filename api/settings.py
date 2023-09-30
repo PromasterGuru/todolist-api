87952c690ab4eb2c1cf538dd2bdc9c4d46f946c7
promasterguru <pmutondo12@gmail.com>
2023-09-30 13:24:19 +0300

    Update git workflow

--- api/settings.py
+++ api/settings.py
@@ -82,8 +82,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER':  os.environ.get('DB_USER'),
         'PASSWORD':  os.environ.get('DB_PASS'),
-        'HOST':  os.environ.get('HOST'),
-        'PORT': '5432',
     }
 }
 
