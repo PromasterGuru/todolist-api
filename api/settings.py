c15158e0ce7c60f2fd49971f9d444a66c35bbb0b
promasterguru <pmutondo12@gmail.com>
2023-09-30 12:43:02 +0300

    Update git workflow

--- api/settings.py
+++ api/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME'),
         'USER':  os.environ.get('DB_USER'),
-        # 'PASSWORD':  os.environ.get('DB_PASS'),
+        'PASSWORD':  os.environ.get('DB_PASS'),
         'HOST':  os.environ.get('HOST'),
         'PORT': '5432',
     }
