5ed00244cbdab19a9af149be41013ae7c82a4406
promasterguru <pmutondo12@gmail.com>
2023-10-12 00:29:50 +0300

    Change port

--- api/settings.py
+++ api/settings.py
@@ -77,13 +77,21 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.environ.get('DB_NAME'),
+    #     'USER':  os.environ.get('DB_USER'),
+    #     'PASSWORD':  os.environ.get('DB_PASS'),
+    #     'HOST':  os.environ.get('DB_HOST'),
+    #     'PORT':  os.environ.get('DB_PORT'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER':  os.environ.get('DB_USER'),
-        'PASSWORD':  os.environ.get('DB_PASS'),
-        'HOST':  os.environ.get('DB_HOST'),
-        'PORT':  os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5433',  # Use the mapped port
     }
 }
 
